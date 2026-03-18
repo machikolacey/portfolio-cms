f11c75783ede53ae4a8c64b06bdbdb6a3441afd3
Kim Kimura <kim.kimura@storm12.co.uk>
2026-03-18 22:54:46 +0000

    Allowed host fixed in settings.py

--- django-api/config/settings.py
+++ django-api/config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-l4*l+l^ypvwzu4&4^o6lnd@xbabz0-#i&=f30o+8l^ktdog%1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [CSRF_TRUSTED_ORIGINS = ['https://web-production-aa2fa.up.railway.app']]
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-aa2fa.up.railway.app']
 
 
 # Application definition
