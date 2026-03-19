053450a2aee92b0cdf0843661b388c55d5c956c6
Kim Kimura <kim.kimura@storm12.co.uk>
2026-03-19 15:36:06 +0000

    added images

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
